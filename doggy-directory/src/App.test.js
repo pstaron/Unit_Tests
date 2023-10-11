c25350f5a9d9172126938acf0a9d8e04c1bbbcae
zakaz <piotr.staron@flask.one>
2023-10-11 20:00:00 +0200

    Add tests

--- doggy-directory/src/App.test.js
+++ doggy-directory/src/App.test.js
@@ -3,4 +3,7 @@ import App from "./App";
 
 test("renders the landing page", () => {
   render(<App />);
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+
 });
