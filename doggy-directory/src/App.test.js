06e4aa87a10bc0976e6457517aa65d696eecc35a
zakaz <piotr.staron@flask.one>
2023-10-11 20:00:12 +0200

    Add more tests

--- doggy-directory/src/App.test.js
+++ doggy-directory/src/App.test.js
@@ -5,5 +5,6 @@ test("renders the landing page", () => {
   render(<App />);
   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
-
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
 });
